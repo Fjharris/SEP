193b12648ccb9ba70f0976b01052ff292c6a5ef0
fjharris <fraser.h@buv.edu.vn>
2021-08-11 15:26:41 +0700

    Reading from database

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,51 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    renderGuides()
+    renderReviews()
 });
 
-function renderGuides()
+//This is the function that gets all the reviews from the database and displays them this is the default function that lists all of the different restaurants
+function renderReviews()
 {
 const db = firebase.firestore()
 db.collection("restaurants").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
 
-        let li= document.createElement('li');
-        let title= document.createElement('span');
-        let rating = document.createElement('span');
+        let divcontainer= document.createElement('div');
+        divcontainer.className = "card-container sub-container"
+        let img = document.createElement('img');
+        img.src = "images/restaurant1.jpg" ;
+        img.alt= "restaurant" ;
+        let crdtext = document.createElement('div');
+        crdtext.className="card-txt";
+        let rname = document.createElement('H1');
+        rname.className="restName";
+        rname.innerHTML = '<a href="restaurantPage.html"> ' +  doc.data().name + ' </a>';
+        let link = document.createElement('a');
+        link.href = "restaurantPage.html";
+        let rlocation = document.createElement('h2');
+        rlocation.className="slant";
+        rlocation.innerText = doc.data().location
 
-        li.setAttribute('data-id',doc.id)
-        title.textContent = doc.data().name;
-        rating.textContent = doc.data().rating;
 
-        li.appendChild(title)
-        li.appendChild(rating)
-        document.getElementById("Restaurants").appendChild(li)
+
+        rname.appendChild(link);
+        divcontainer.appendChild(img);
+        crdtext.appendChild(rname);
+        crdtext.appendChild(rlocation);
+        divcontainer.appendChild(crdtext)
+
+
+
+
+
+//        let title= document.createElement('span');
+//        let rating = document.createElement('span');
+
+//        li.setAttribute('data-id',doc.id)
+//      title.textContent = doc.data().name;
+//        rating.textContent = doc.data().rating;
+
+        document.getElementById("startofreviews").append(divcontainer)
 
 
     });
